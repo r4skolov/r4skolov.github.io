3c4e480a4ff2efd9ec583557cdb83e8e51a188e1
r4skolov <davydchnkov1@gmail.com>
2021-01-02 23:31:02 +0300

    update

--- pahu/js/main.js
+++ pahu/js/main.js
@@ -163,3 +163,18 @@ $(window).on('load resize', function() {
     $(".test-slider.slick-initialized").slick("unslick");
   }
 });
+
+document.body.onload = function() {
+  setTimeout(function() {
+    var preloader = document.getElementById('page-preloader');
+    if( !preloader.classList.contains('done'))
+    {
+      preloader.classList.add('done');
+      document.body.classList.remove('hide-scroll');
+    }
+    
+  },1000);
+  $('.preloader').on('wheel', function (e) {
+    e.stopPropagation();
+  })
+};
