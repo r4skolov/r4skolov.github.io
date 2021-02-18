a3114066387cf143f26af99b4694d792aa1e2843
r4skolov <davydchnkov1@gmail.com>
2021-02-18 05:14:50 +0300

    new

---
+++ mars/js/main.js
@@ -0,0 +1,81 @@
+$("[data-collapse]").on("click", function(event) {
+    event.preventDefault();
+
+    var $this = $(this);
+        blockid = $this.data('collapse');
+        $this.toggleClass('active');
+        $('.answers_toggle').toggleClass('active');
+        $(blockid).slideToggle();
+});
+
+
+
+/* slider */
+
+$(function(){ 
+    $('.intro_slider').slick({
+        infinite: true,
+        adaptiveHeight: true,
+        touchMove:true,
+        speed: 700,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth:false,
+        autoplay: false,
+        prevArrow: '.intro-btn__prev',
+        nextArrow: '.intro-btn__next',
+    })
+    $(".intro_slider").on('afterChange', function(event, slick, currentSlide){
+        $(".countsSlides_1").text(currentSlide + 1);
+     });
+    
+
+
+ $('.teacher_slider').slick({
+    infinite: true,
+    adaptiveHeight: true,
+    touchMove:true,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth:true,
+     prevArrow: '.teacher__btn',
+    nextArrow:  '.teacher__btn ',
+    })
+    $(".teacher_slider").on('afterChange', function(event, slick, currentSlide){
+        $(".countsSlides_2").text(currentSlide + 1);
+    });
+
+    $('.concerts_slider').slick({
+        infinite: true,
+        adaptiveHeight: true,
+        touchMove:true,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth:true,
+        prevArrow: ' .concerts__btn',
+        nextArrow: ' .concerts__btn',
+    })
+
+    $(".concerts_slider").on('afterChange', function(event, slick, currentSlide){
+        $(".countsSlides_3").text(currentSlide + 1);
+     });
+});
+ 
+
+
+
+
+
+$("#burger").on("click", function(event) {
+    event.preventDefault();
+  
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
+    $('body').toggleClass('lock');
+  
+    $('#nav').on('wheel', function (e) {
+      e.stopPropagation();
+    });
+  });
