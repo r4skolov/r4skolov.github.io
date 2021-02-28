8dc6e4d9d0192120a94af15a1233b760be9e326d
r4skolov <davydchnkov1@gmail.com>
2021-03-01 01:14:24 +0300

    Update main.js

--- status-stroy/js/main.js
+++ status-stroy/js/main.js
@@ -98,3 +98,50 @@ $(window).on('load resize', function() {
       $(".clients-slider.slick-initialized").slick("unslick");
     }
   });
+
+
+  const CLASS_LIST = {
+    MODAL: 'modal',
+    MODAL_ACTIVE: 'modal--active',
+    MODAL_HAS_SCROLL: 'modal--has-scroll',
+    MODAL_DIALOG_BODY: 'modal__dialog-body',
+    TRIGGER_OPEN: 'js-modal-open',
+    TRIGGER_CLOSE: 'js-modal-close'
+    
+};
+
+
+
+document.addEventListener('click', (event) => {
+    //open
+    if (event.target.closest(`.${CLASS_LIST.TRIGGER_OPEN}`)) {
+      console.log('open');
+      event.preventDefault();
+
+      const target = event.target.closest(`.${CLASS_LIST.TRIGGER_OPEN}`);
+      const modalId = target.getAttribute('href').replace('#', '');
+      const modal = document.getElementById(modalId);
+
+      document.body.style.overflow = 'hidden';
+
+      modal.classList.add(CLASS_LIST.MODAL_ACTIVE);
+    }
+
+    if ( 
+      event.target.closest(`.${CLASS_LIST.TRIGGER_CLOSE}`) ||
+      event.target.classList.contains(CLASS_LIST.MODAL_ACTIVE)
+    ) {
+      console.log('close');
+      event.preventDefault();
+
+      const modal = event.target.closest(`.${CLASS_LIST.MODAL}`);
+      modal.classList.remove(CLASS_LIST.MODAL_ACTIVE);
+
+      document.body.style.overflow = 'visible';
+    }
+
+    $('.modal').on('wheel', function (e) {
+      e.stopPropagation();
+  })
+
+});
