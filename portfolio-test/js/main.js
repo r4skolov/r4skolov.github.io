af1767d94acf5472401a25fb6598ee24da1d61cf
r4skolov <davydchnkov1@gmail.com>
2021-03-23 08:05:15 +0300

    update

---
+++ portfolio-test/js/main.js
@@ -0,0 +1,14 @@
+document.body.onload = function() {
+    setTimeout(function() {
+      var preloader = document.getElementById('page-preloader');
+      if( !preloader.classList.contains('done'))
+      {
+        preloader.classList.add('done');
+        document.body.classList.remove('hide-scroll');
+      }
+      
+    },1000);
+    $('.preloader').on('wheel', function (e) {
+      e.stopPropagation();
+    })
+  };
